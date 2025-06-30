e2f410acfe3c941eebb3d8bc252a0e1d1ca3f253
Michiko-01 <cheong.michiko@gmail.com>
2025-06-30 23:37:43 +0200

    Remove headless mode from browser launch in global setup

--- global-setup.ts
+++ global-setup.ts
@@ -7,7 +7,7 @@ dotenv.config();
 //Login and save session for authentication
 async function globalSetup(config: FullConfig) {
   const { baseURL, storageState } = config.projects[0].use;
-  const browser = await chromium.launch({ headless: false });
+  const browser = await chromium.launch();
   const page = await browser.newPage();
   await page.goto(baseURL! + '/login', { waitUntil: 'networkidle' });
 
